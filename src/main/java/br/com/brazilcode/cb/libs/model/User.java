68435de877da113e1faa0d112d70dabb56e12a98
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-02-21 11:00:15 -0300

    Criando a base inicial do Model

---
+++ src/main/java/br/com/brazilcode/cb/libs/model/User.java
@@ -0,0 +1,241 @@
+package br.com.brazilcode.cb.libs.model;
+
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * Classe responsável por mapear a tabela "USER" do banco de dados.
+ *
+ * @author Brazil Code - Gabriel Guarido
+ * @since 20 de fev de 2020 19:46:28
+ * @version 1.0
+ */
+@Entity(name = "User")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+	@GenericGenerator(name = "user_seq", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+			@Parameter(name = "sequence_name", value = "user_seq"), @Parameter(name = "initial_value", value = "1"),
+			@Parameter(name = "increment_size", value = "1") })
+	private int id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Area area;
+
+	@NotEmpty
+	@Column(length = 50)
+	private String username;
+
+	@NotEmpty
+	@Column(length = 50)
+	private String password;
+
+	@NotEmpty
+	@Column(length = 30)
+	private String firstName;
+
+	@Column(length = 30)
+	private String lastName;
+
+	@NotEmpty
+	@Column(length = 100)
+	private String email;
+
+	private String token;
+
+	@NotEmpty
+	private boolean disabled;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_profile", joinColumns = { @JoinColumn(name = "id_user") }, inverseJoinColumns = {
+			@JoinColumn(name = "id_profile") })
+	private Set<Profile> profiles = new HashSet<>();
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Timestamp updatedAt;
+
+	@NotEmpty
+	@Temporal(TemporalType.TIMESTAMP)
+	private Timestamp createdAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Area getArea() {
+		return area;
+	}
+
+	public void setArea(Area area) {
+		this.area = area;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public boolean isDisabled() {
+		return disabled;
+	}
+
+	public void setDisabled(boolean disabled) {
+		this.disabled = disabled;
+	}
+
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = prime * result + (disabled ? 1231 : 1237);
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((token == null) ? 0 : token.hashCode());
+		result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (createdAt == null) {
+			if (other.createdAt != null)
+				return false;
+		} else if (!createdAt.equals(other.createdAt))
+			return false;
+		if (disabled != other.disabled)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (token == null) {
+			if (other.token != null)
+				return false;
+		} else if (!token.equals(other.token))
+			return false;
+		if (updatedAt == null) {
+			if (other.updatedAt != null)
+				return false;
+		} else if (!updatedAt.equals(other.updatedAt))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/br/com/brazilcode/cb/libs/util/DateUtil.java
@@ -0,0 +1,99 @@
+package br.com.brazilcode.cb.libs.util;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * Classe utilitária para formatação de datas.
+ *
+ * @author Brazil Code - Gabriel Guarido
+ * @since 21 de fev de 2020 08:39:54
+ * @version 1.0
+ */
+public class DateUtil {
+
+	private static final String PATTERN_YYYYMMDD = "yyyyMMdd";
+	private static final String PATTERN_DDMMYYYY = "ddMMyyyy";
+
+	private static final DateTimeFormatter FORMATTER_YYYYMMDD = DateTimeFormatter.ofPattern(PATTERN_YYYYMMDD);
+	private static final DateTimeFormatter FORMATTER_DDMMYYYY = DateTimeFormatter.ofPattern(PATTERN_DDMMYYYY);
+
+	/**
+	 * Método responsável por converter um objeto do tipo LocalDate para Date.
+	 *
+	 * @author Brazil Code - Gabriel Guarido
+	 * @param localDate
+	 * @return
+	 */
+	public static Date toDate(LocalDate localDate) {
+		Date returnDate = null;
+		if (localDate != null) {
+			returnDate = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		}
+
+		return returnDate;
+	}
+
+	/**
+	 * Método responsável por converter um objeto do tipo Date para LocalDate.
+	 *
+	 * @author Brazil Code - Gabriel Guarido
+	 * @param date
+	 * @return
+	 */
+	public static LocalDate toLocalDate(Date date) {
+		LocalDate returnDate = null;
+		if (date != null) {
+			returnDate = Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+		}
+
+		return returnDate;
+	}
+
+	/**
+	 * Método responsável por converter um objeto do tipo Date em String com o seguite pattern: 'yyyyMMdd'.
+	 *
+	 * @author Brazil Code - Gabriel Guarido
+	 * @param date
+	 * @return
+	 */
+	public static String convertDateToStringPatternYyyyMmDd(Date date) {
+		String returnDate = null;
+		if (date != null) {
+			returnDate = toLocalDate(date).format(FORMATTER_YYYYMMDD);
+		}
+
+		return returnDate;
+	}
+
+	/**
+	 * Método responsável por converter um objeto do tipo Date em String com o seguite pattern: 'ddMMyyyy'.
+	 *
+	 * @author Brazil Code - Gabriel Guarido
+	 * @param date
+	 * @return
+	 */
+	public static String convertDateToStringPatternDdMmYyyy(Date date) {
+		String returnDate = null;
+		if (date != null) {
+			returnDate = toLocalDate(date).format(FORMATTER_DDMMYYYY);
+		}
+
+		return returnDate;
+	}
+
+	/**
+	 * Método responsável por retornar um objeto do tipo Date com a data atual.
+	 *
+	 * @author Brazil Code - Gabriel Guarido
+	 * @return
+	 */
+	public static Date getDateNow() {
+		return Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+}
