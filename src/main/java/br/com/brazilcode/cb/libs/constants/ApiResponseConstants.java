45848bbe021c02daea1bb3a95d12e97a58fdb995
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-03-29 17:15:16 -0300

    Implementando task de atualização de usuário

--- src/main/java/br/com/brazilcode/cb/libs/constants/ApiResponseConstants.java
+++ src/main/java/br/com/brazilcode/cb/libs/constants/ApiResponseConstants.java
@@ -5,12 +5,13 @@ package br.com.brazilcode.cb.libs.constants;
  *
  * @author Brazil Code - Gabriel Guarido
  * @since 8 de mar de 2020 16:38:48
- * @version 1.0
+ * @version 1.1
  */
 public class ApiResponseConstants {
 
 	public static final String INTERNAL_SERVER_ERROR_RESPONSE = "Unexpected Internal Error";
 	public static final String CREATED_RESPONSE = "Registered Successfully";
+	public static final String UPDATED_RESPONSE = "Updated Successfully";
 	public static final String VALIDATION_ERROR_RESPONSE = "Validation Error";
 	public static final String REST_INTEGRATION_ERROR = "Unexpected error on trying to comunicate with external REST API";
 
